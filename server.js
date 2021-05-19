99436411ec1da597df5e14daa37edd9c3d802392
alishahwee <alisha.hwee@outlook.com>
2021-05-18 23:00:30 -0500

    Create dynamic date endpoint

--- server.js
+++ server.js
@@ -23,6 +23,36 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// date endpoint
+app.get('/api/:date?', (req, res) => {
+  let date = new Date();
+
+  if (!req.params.date) {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
+  } else {
+    try {
+      date = new Date(req.params.date);
+      if (date.toString() === 'Invalid Date') {
+        date = new Date(Number(req.params.date));
+        res.json({
+          unix: date.getTime(),
+          utc: date.toUTCString(),
+        });
+      } else {
+        res.json({
+          unix: date.getTime(),
+          utc: date.toUTCString(),
+        });
+      }
+    } catch (err) {
+      res.json({ error: 'Invalid date' });
+    }
+  }
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
